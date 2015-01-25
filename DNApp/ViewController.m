81285f6d03ea751c8fb0b83ac0bb36c65c436088
Vivek Bhatia <bhatiavn@gmail.com>
2015-01-25 11:33:27 -0800

    Initial Commit

--- DNApp/ViewController.m
+++ DNApp/ViewController.m
@@ -17,11 +17,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setNeedsStatusBarAppearanceUpdate];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)unwindFromView:(UIStoryboardSegue*)segue {
+    
+}
+
 
 @end
